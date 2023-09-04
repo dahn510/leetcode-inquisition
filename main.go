6b5ba496f7a96623663e30bea811fdde14c72e89
dahn510 <dahn510@gmail.com>
2023-09-04 08:51:36 -0700

    only fetch non-premium problems

--- main.go
+++ main.go
@@ -11,23 +11,29 @@ import (
 )
 
 func main() {
-	problems, err := leetcodeapi.GetAllProblems(rand.Intn(1000), 1)
-	if err != nil {
-		log.Fatal(err)
+	// get non-premium problems
+	var problem leetcodeapi.Problem
+	for {
+		problems, err := leetcodeapi.GetAllProblems(rand.Intn(1000), 1)
+		if err != nil {
+			log.Fatal(err)
+		}
+		problem = problems.Problems[0]
+		if !problem.PaidOnly {
+			break
+		}
 	}
-
-	problem := problems.Problems[0]
 
 	link := fmt.Sprintf("https://leetcode.com/problems/%s", problem.TitleSlug)
-	fmt.Printf("Title: %s Difficulty: %s\n%s\n\n", problem.TitleSlug, problem.Difficulty, link)
+	fmt.Printf("Title: %s\nDifficulty: %s\n%s\n\n", problem.Title, problem.Difficulty, link)
 	
-	contents, err := leetcodeapi.GetProblemContentByTitleSlug(problems.Problems[0].TitleSlug)
+	contents, err := leetcodeapi.GetProblemContentByTitleSlug(problem.TitleSlug)
 	if err != nil {
 		log.Fatal(err)
 	}
 
+	// convert problem in html to markdown
 	converter := md.NewConverter("", true, nil)
-
 	markdown, err := converter.ConvertString(contents.Content)
 	if err != nil {
 		log.Fatal(err)
